dd3e71fcf675a856c63771d2930dbffdb482b264
Harald Pehl <harald.pehl@gmail.com>
2015-09-17 10:31:43 +0200

    Copy fonts to tour/fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,40 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
+    copy = require('gulp-copy'),
     less = require('gulp-less'),
     plumber = require('gulp-plumber'),
     browserSync = require('browser-sync'),
     reload = browserSync.reload;
 
-// Uglyfies js on to /js/minjs
-gulp.task('scripts', function(){  
-  gulp.src('js/*.js')
-    .pipe(plumber())
-    .pipe(uglify())
-    .pipe(gulp.dest("js/minjs"));
-}); 
+// Uglyfies js on to /tour/js/minjs
+gulp.task('scripts', function () {
+    gulp.src('js/*.js')
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest("tour/js/minjs"));
+});
 
-// Compiles less on to /css
+// Copy fonts to /tour/fonts to become part of the distribution
+gulp.task('copy', function () {
+    gulp.src('bower_components/patternfly/dist/fonts/*')
+        .pipe(copy('tour/fonts', {prefix: 4}));
+    gulp.src('bower_components/patternfly/components/font-awesome/fonts/*')
+        .pipe(copy('tour/fonts', {prefix: 5}));
+    gulp.src('bower_components/patternfly/components/bootstrap/dist/fonts/*')
+        .pipe(copy('tour/fonts', {prefix: 6}));
+});
+
+// Compiles less on to /tour/css
 gulp.task('less', function () {
-  gulp.src('less/**/*.less')
-   .pipe(plumber())
-   .pipe(less())
-   .pipe(gulp.dest('css'))
-   .pipe(reload({stream:true}));
+    gulp.src('less/**/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest('tour/css'))
+        .pipe(reload({stream: true}));
 });
 
 // reload server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync({
         server: {
             baseDir: "./"
@@ -37,11 +48,12 @@ gulp.task('bs-reload', function () {
 });
 
 // watch for changes on files
-gulp.task('watch', function(){ 
-  gulp.watch('js/*.js', ['scripts']);
-  gulp.watch('less/*.less', ['less']);
-  gulp.watch("*.html", ['bs-reload']);
-}); 
+gulp.task('watch', function () {
+    gulp.watch('js/*.js', ['scripts']);
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch("index.html", ['bs-reload']);
+    gulp.watch("tour/**/*.html", ['bs-reload']);
+});
 
-// deploys
-gulp.task('default',  ['scripts', 'less','browser-sync','watch']);
+gulp.task('package', ['scripts', 'copy', 'less']);
+gulp.task('default', ['scripts', 'copy', 'less', 'browser-sync', 'watch']);
